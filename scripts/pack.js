7188778ceff00e171b475a548691e2585a39e16a
Entkenntnis <Entkenntnis@users.noreply.github.com>
2021-03-17 14:22:38 +0100

    polishing

--- scripts/pack.js
+++ scripts/pack.js
@@ -1,14 +1,19 @@
 const fs = require('fs')
 const { PNG } = require('pngjs')
 
+const fileRegex = /^exercise[\d]+\.ts$/
+const layerRegex = /\{ src: '([^']+)'(?: |, offset: ([\d]+) )}/g
+
 const exercises = fs.readdirSync('./src/data')
-const regex = /\{ src: '([^']+)'(?: |, offset: ([\d]+) )}/g;
+
+console.log(exercises)
 
 for (const exercise of exercises) {
-  if (/^exercise[\d]+\.ts$/.test(exercise)) {
+  if (fileRegex.test(exercise)) {
     const file = './src/data/' + exercise
+    console.log(file)
     const code = fs.readFileSync(file, 'utf-8')
-    const newCode = code.replace(regex, (match, src, offset) => {
+    const newCode = code.replace(layerRegex, (match, src, offset) => {
       console.log({ src, offset })
       
       // read png, crop, and adjust offset
@@ -38,16 +43,18 @@ for (const exercise of exercises) {
         cropBottom++
       }
       
-      const newHeight = png.height - cropTop * 47 - cropBottom
-      const dst = new PNG({ width: png.width, height: newHeight })
-      PNG.bitblt(png, dst, 0, cropTop * 47, png.width, newHeight, 0, 0)
-      
-      const outputBuffer = PNG.sync.write(dst)
-      
-      fs.writeFileSync('./public' + src, outputBuffer)
+      if (cropTop > 0 || cropBottom > 0) {
+        console.log({cropTop, cropBottom})
+        const newHeight = png.height - cropTop * 47 - cropBottom
+        const dst = new PNG({ width: png.width, height: newHeight })
+        PNG.bitblt(png, dst, 0, cropTop * 47, png.width, newHeight, 0, 0)
+        const outputBuffer = PNG.sync.write(dst)
+        fs.writeFileSync('./public' + src, outputBuffer)
+      } else {
+        console.log('Alles ok')
+      }
       
       const newOffset = parseInt(offset || '0') + cropTop
-      
       const replacement = `{ src: '${src}'${newOffset ? `, offset: ${newOffset} ` : ' '}}`
       console.log(replacement)
       return replacement
